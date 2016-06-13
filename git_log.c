e3a675cfebc936fd1326b1e17e41eca0ad3ba264
leothedeveloper <leothedeveloper@outlook.com>
2016-06-13 14:56:44 +0800

    second commit

--- git_log.c
+++ git_log.c
@@ -1,5 +1,5 @@
-#define LOG_BUF_MAX 1
+#define LOG_BUF_MAX 2
 unsigned long git_deflate_bound(z_streamp strm, unsigned long size)
 {
-  return deflateBound(strm, size);
+  return deflateBound(&strm->z, size);
 }
